8cc1a7c5908a213e54fbe66bf3e958cc24657211
Saksham Gupta <sakshamgupta@Sakshams-MBP.hsd1.il.comcast.net>
2021-06-25 22:13:45 -0500

    init screens

--- screens/Routes.js
+++ screens/Routes.js
@@ -5,9 +5,28 @@ import { createBottomTabsNavigator } from "@react-navigation/bottom-tabs"
 import { MaterialCommunityIcons } from "@expo/vector-icons"
 import colors from "../config/colors"
 
+const Tabs = createBottomTabsNavigator()
+
+function Home(){
+
+}
+
+function Logout(){
+
+}
+
 function Routes() {
     return (
-        
+        <NavigationContainer>
+            <Tabs.Navigator initialRoute="Home"
+            screenOptions={({route}) => ({
+                tabBarIcon: ({focused, color, size}) => {
+                    let iconName
+                }
+            })}>
+
+            </Tabs.Navigator>
+        </NavigationContainer>
     )
 }
 
