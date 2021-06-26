082a3918d972da836ad13a1d8da22ed5cabff777
Saksham Gupta <sakshamgupta@Sakshams-MBP.hsd1.il.comcast.net>
2021-06-26 14:51:32 -0500

    home

--- screens/QwttrHomeScreen.js
+++ screens/QwttrHomeScreen.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
-// import { Icon } from 'react-native-vector-icons/Icon'
+import { FlatList, TouchableWithoutFeedback } from "react-native-gesture-handler"
 import { Icon } from "react-native-elements"
 import colors from '../config/colors';
+import { db } from '../config/Firebase';
+import Post from '../components/Post';
 
 const QwttrHomeScreen = () => {
 
-    
+    const [data, setData] = useState()
+
+    const getData = async () => {
+        const snapshot = await db.collection('posts').get()
+        setData(snapshot.docs.map(doc => doc.data()))
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
 
     return (
-        <SafeAreaView style={styles.container}>
+        <SafeAreaView>
             <View style={styles.flex}>
                 <Text style={styles.largeText}>Home</Text>
                 <View style={styles.icon}
@@ -26,24 +37,39 @@ const QwttrHomeScreen = () => {
                     />
                 </View>
             </View>
+            <View style={styles.posts}>
+                <FlatList
+                    data={data}
+                    renderItem={({ item }) => {
+                        return (
+                            <Post item={item} />
+                        )
+                    }}
+                    keyExtractor={item => item.uid}
+                />
+            </View>
+
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        marginLeft: 20,
-    },
     icon: {
         marginTop: 5,
-        marginRight: 10
     },
     flex: {
         flexDirection: 'row',
-        justifyContent: 'space-between'
+        justifyContent: 'space-between',
+        marginLeft: 15,
+        marginRight: 10
     },
     largeText: {
         fontSize: 60
+    },
+    posts: {
+        marginLeft: 15,
+        marginRight: 20,
+        marginTop: 10
     }
 })
 
